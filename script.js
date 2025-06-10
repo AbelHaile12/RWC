84716b74ea6eb9841e4dda36b00c2f218d9b1a48
AbelHaile12 <abelhaile68@gmail.com>
2025-06-09 21:55:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 function subscribe(plan) {
     alert("Redirecting to Stripe checkout for " + plan + " plan.");
 }
+function showFeature() {
+  const selected = document.getElementById("featureSelect").value;
+  const boxes = document.getElementsByClassName("feature-box");
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].style.display = "none";
+  }
+
+  if (selected !== "none") {
+    document.getElementById(selected).style.display = "block";
+  }
+}
